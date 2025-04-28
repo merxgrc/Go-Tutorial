e5049bd1c1c8d8d3d88821343cfd4b874eaf2483
merxgrc <mgarcia73@mail.sfsu.edu>
2025-04-27 18:21:21 -0700

    added a list function

--- hello_world.go
+++ hello_world.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func list() {
+	fmt.Println("\nYo this is the list function")
+}
+
 func main() {
 
 	// assign string "yo" to a and print it
@@ -12,5 +16,7 @@ func main() {
 	const b int32 = 12
 	fmt.Println("b:", b)
 	fmt.Printf("b-type: %T ", b)
+
+	list()
 
 }
