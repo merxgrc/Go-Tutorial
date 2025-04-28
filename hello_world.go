6657af22e8bf646ce0622b76a83cd0ddb5399d97
merxgrc <mgarcia73@mail.sfsu.edu>
2025-04-27 18:13:09 -0700

    initial commit

---
+++ hello_world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// assign string "yo" to a and print it
+	a := "yo"
+	fmt.Println(a)
+
+	// assign types and print
+	const b int32 = 12
+	fmt.Println("b:", b)
+	fmt.Printf("b-type: %T ", b)
+
+}
