4ca7c4b114f289e69ff10e613d1f630d52c68ff3
merxgrc <mgarcia73@mail.sfsu.edu>
2025-04-27 18:31:57 -0700

    Cleaner for loop

--- hello_world.go
+++ hello_world.go
@@ -6,8 +6,11 @@ func list() {
 	// Creates a list of ints and prints out the index and the value
 	fmt.Println("\nYo this is the list function")
 	var myList = [3]int32{1, 2, 3}
-	for i := 0; i < len(myList); i++ {
-		fmt.Println(i, myList[i])
+	// for i := 0; i < len(myList); i++ {
+	// 	fmt.Println(i, myList[i])
+	// }
+	for i, nums := range myList {
+		fmt.Println(i, nums)
 	}
 }
 
