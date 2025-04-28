8371b591ce7997639bddb4b1a07fc76f4e3a16c9
merxgrc <mgarcia73@mail.sfsu.edu>
2025-04-27 18:28:30 -0700

    Added list function code to Create a list, travers, and print out the index and the value

--- hello_world.go
+++ hello_world.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func list() {
+	// Creates a list of ints and prints out the index and the value
 	fmt.Println("\nYo this is the list function")
+	var myList = [3]int32{1, 2, 3}
+	for i := 0; i < len(myList); i++ {
+		fmt.Println(i, myList[i])
+	}
 }
 
 func main() {
